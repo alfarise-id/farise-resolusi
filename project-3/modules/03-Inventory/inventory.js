5261e130299cd22c9f279b071cfc18391a12716e
alfarise-id <syamsialfarisii@gmail.com>
2026-02-06 23:21:04 +0700

    Project 3: Memperbaiki sistem inventory dari awal dengan penulisan yang lebih rapih dan terstruktur

--- project-3/modules/03-Inventory/inventory.js
+++ project-3/modules/03-Inventory/inventory.js
@@ -1,115 +1,159 @@
-const form_np = document.querySelector(".new_product");
-const date = document.querySelector("#date");
-const name_p = document.querySelector("#name");
-const unit = document.querySelector("#unit");
-const min_stock = document.querySelector("#min_stock");
-const qty = document.querySelector("#qty");
-const price = document.querySelector("#price");
-const rupiah = (x) => new Intl.NumberFormat('id-ID', {
-    style: "currency",
-    currency: "IDR",
+/* =========================================
+   1. CONSTANTS 
+   ========================================= */
+const SK_LP = "list_produk";
+const SK_RE = "detail_produk";
+
+/* =========================================
+   2. STATE VARIABLES 
+   ========================================= */
+let isEditing = false;     
+let editIndex = null;       
+
+/* =========================================
+   3. DOM ELEMENTS 
+   ========================================= */
+const sec_inputLp       = document.querySelector('#inputLp'); // INDEX
+const formLp            = document.querySelector('#formLp'); 
+const input_dateLp      = document.querySelector('#dateLp');
+const input_nameLp      = document.querySelector('#nameLp');
+const input_unitLp      = document.querySelector('#unitLp');
+const input_minstockLp  = document.querySelector('#min_stockLp');
+const input_qtyLp       = document.querySelector('#qtyLp');
+const input_priceLp     = document.querySelector('#priceLp');
+const tbodyLp           = document.querySelector("#tbodyLp");
+
+const sec_inputRe       = document.querySelector('#inputRe'); // RESTOCK
+const formRe            = document.querySelector('#formRe'); 
+const input_dateRe      = document.querySelector('#dateRe');
+const input_typeRe      = document.querySelector('#typeRe');
+const input_qtyRe       = document.querySelector('#qtyRe');
+const input_priceRe     = document.querySelector('#priceRe');
+const input_notesRe     = document.querySelector('#notesRe');
+const tbodyRe           = document.querySelector("#tbodyRe");
+
+/* =========================================
+   4. FUNCTIONS 
+   ========================================= */
+const formatRupiah = (angka) => new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
     minimumFractionDigits: 0
-}).format(Number(x));
+}).format(Number(angka));
 
-
-// FUNCTION
-const action_button = () => {
-    const hapus = document.querySelector('#delete');
-    const edit = document.querySelector('#edit');
-    const detail = document.querySelector('#detail');
-    if (hapus) {
-        hapus.addEventListener("click", (e)=> {
-            const tr_hapus = e.target.closest('tr');
-            const id_product_hapus = tr_hapus.getAttribute("index");
-
-            let list_p_hapus = JSON.parse(localStorage.getItem("list_p"));
-            let list_p_baru = list_p_hapus.splice(Number(id_product_hapus), 1);
-            let detail_p_hapus = JSON.parse(localStorage.getItem("detail_p"));
-            let detail_p_baru = detail_p_hapus.filter(item => item.id_product !== Number(id_product_hapus));
-            localStorage.setItem("list_p", JSON.stringify(list_p_baru));
-            localStorage.setItem("detail_p", JSON.stringify(detail_p_baru));
-            renderlp();
-        })
-    }
-}   
-
-const createTD = (isi) => {
-    const td = document.createElement("td");
-    td.textContent = isi;
+const createTd = (item) => {
+    const td = document.createElement('td');
+    td.textContent = item;
     return td;
 }
 
-const renderlp = () => {
-    let list_p = JSON.parse(localStorage.getItem("list_p")) || [];
-    let tbody = document.querySelector(".table_lp table tbody");
+const renderTable = () => {
+    if (formLp) {
+        const storageLp = JSON.parse(localStorage.getItem(SK_LP)) || [];
+        tbodyLp.textContent = '';
 
-    tbody.textContent = "";
+        storageLp.forEach(item => {
+            const tr = document.createElement('tr');
 
+            tr.append(
+                createTd(item.id),
+                createTd(item.name),
+                createTd(item.unit),
+                createTd(item.min_stock),
+                createTd(item.qty),
+                createTd(formatRupiah(item.price))
+            )
 
-    list_p.forEach((item,i) => {
-        const tr = document.createElement("tr");
+            const td_btnLp = document.createElement('td');
+            td_btnLp.innerHTML = `
+            <button id='deleteLp'>Delete</button>
+            <button id='editLp'>Edit</button>
+            <button id='detaLp'>Detail</button>`;
 
-        tr.setAttribute("index", i);
-
-        tr.append(
-            createTD(i),
-            createTD(item.name),
-            createTD(item.unit),
-            createTD(item.min_stock),
-            createTD(item.current_qty),
-            createTD(rupiah(item.avg_price))
-        )
-
-        const td_action = document.createElement("td");
-        td_action.innerHTML = "<button id='delete'>Delete</button><button id='edit'>Edit</button><button id='detail'>Detail</button>";
-
-        tr.appendChild(td_action);
-        tbody.appendChild(tr);
-        action_button();
-    });
+            tr.appendChild(td_btnLp);
+            tbodyLp.appendChild(tr);
+        });
+    } else if (formRe) {
+        
+    }
 }
-// FUNCTION
 
-// LISTENER
-if (form_np) {
-    form_np.addEventListener("submit", (e) => {
+const simpanDataLp = (data) => {
+    if (isEditing) {
+        return alert("Not Yet")
+    }
+    localStorage.setItem(SK_LP, JSON.stringify(data));
+}
+
+const simpanDataRe = (data) => {
+    if (isEditing) {
+        return alert("Not Yet")
+    }
+    
+    localStorage.setItem(SK_RE, JSON.stringify(data));
+}
+/* =========================================
+   5. LISTENERS (Interaksi User)
+   ========================================= */
+if (formLp) {  
+    formLp.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let list_p = JSON.parse(localStorage.getItem("list_p")) || [];
-        let detail_p = JSON.parse(localStorage.getItem("detail_p")) || [];
+        const storageLp       = JSON.parse(localStorage.getItem(SK_LP)) || [];
+        const storageRe       = JSON.parse(localStorage.getItem(SK_RE)) || [];
+        const new_dateLp      = input_dateLp.value;
+        const new_nameLp      = input_nameLp.value.trim();
+        const new_unitLp      = input_unitLp.value;
+        const new_minstockLp  = input_minstockLp.value;
+        const new_qtyLp       = input_qtyLp.value;
+        const new_priceLp     = input_priceLp.value;
 
-        let table_lp = {
-            name: name_p.value,
-            unit: unit.value,
-            min_stock: min_stock.value,
-            current_qty: qty.value,
-            avg_price: price.value,
+        if (Number(new_unitLp) < 0 || Number(new_minstockLp) < 0 || Number(new_qtyLp) < 0 || Number(new_priceLp) < 0 ) {
+            alert("Input Can't Be Lower Than 0!");
+            return;
         }
 
-        list_p.push(table_lp); 
+        const double_nameLp = storageLp.some(item => {
+            return item.name.toLowerCase() === new_nameLp.toLowerCase();
+        })
 
-        localStorage.setItem("list_p", JSON.stringify(list_p));
+        if (double_nameLp) {
+            alert('Name Already Used!');
+            return;
+        }
 
-        let table_dp = {
-            id_product: list_p.length - 1,
-            date: date.value,
+        const new_dataRe = {
+            id: Date.now(),
+            id_product: Date.now(),
+            date: new_dateLp,
             type: "IN",
-            qty: qty.value,
-            price: price.value,
-            balance_qty: qty.value,
-            notes: "Add New Products",
-        }
+            qty: new_qtyLp,
+            price: new_priceLp,
+            balance_qty: new_qtyLp,
+            notes: "Add Product"
+        };
+        
+        const new_dataLp = {
+            id: Date.now(),
+            name: new_nameLp,
+            unit: new_unitLp,
+            min_stock: new_minstockLp,
+            qty: new_qtyLp,
+            price: new_priceLp
+        };
 
-        detail_p.push(table_dp);
+        storageLp.push(new_dataLp);
+        storageRe.push(new_dataRe);
 
-        localStorage.setItem("detail_p", JSON.stringify(detail_p));
+        simpanDataRe(storageRe);
+        simpanDataLp(storageLp);
 
-        renderlp();
-        action_button();
-        form_np.reset();
-    })
+        renderTable();
+        formLp.reset();
+    });
 }
-// LISTENER
-
-renderlp()
-action_button()
+/* =========================================
+   6. INITIALIZATION 
+   ========================================= */
+renderTable();     
+console.log("Aplikasi Siap! 🚀");
