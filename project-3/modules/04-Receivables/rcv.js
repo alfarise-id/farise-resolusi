8927f562e08dcfe7b15f1c5315ed83eca883a7fa
alfarise-id <syamsialfarisii@gmail.com>
2026-02-17 21:41:38 +0700

    Project 3: Melanjutkan pengerjaan js pada module receivables

--- project-3/modules/04-Receivables/rcv.js
+++ project-3/modules/04-Receivables/rcv.js
@@ -7,12 +7,20 @@ let id_tr = null;
 
 const main_rc = document.querySelector('#main_receivable');
 const sec_input_rc = document.querySelector('#input_rc');
+const input_h2_rc = document.querySelector('#input_rc h2');
 const form_rc = document.querySelector('#form_rc');
+const input_dateRc = document.querySelector('#date_rc');
 const input_descRc = document.querySelector('#desc_rc');
 const input_typeRc = document.querySelector('#type_rc');
 const input_amountRc = document.querySelector('#amount_rc');
 const input_interestRc = document.querySelector('#interest_rc');
 const input_termRc = document.querySelector('#term_rc');
+const input_dateRcL = document.querySelector('#date_rcL');
+const input_descRcL = document.querySelector('#desc_rcL');
+const input_typeRcL = document.querySelector('#type_rcL');
+const input_amountRcL = document.querySelector('#amount_rcL');
+const input_interestRcL = document.querySelector('#interest_rcL');
+const input_termRcL = document.querySelector('#term_rcL');
 const sec_tableRc = document.querySelector('#table_rc');
 const tbody_rc = document.querySelector('#tbody_rc');
 
@@ -31,6 +39,7 @@ const formatRupiah = (data) => new Intl.NumberFormat('id-ID', {
 }).format(Number(data));
 
 const submit_rc = () => {
+    const v_date = input_dateRc.value;
     const v_desc = input_descRc.value;
     const v_type = input_typeRc.value;
     const v_amount = input_amountRc.value;
@@ -38,6 +47,7 @@ const submit_rc = () => {
     const v_term = input_termRc.value;
 
     const storage_rc = JSON.parse(localStorage.getItem(SK_RC)) || [];
+    const storage_drc = JSON.parse(localStorage.getItem(SK_DRC)) || [];
 
     const new_data = {
         id: Date.now(),
@@ -49,8 +59,49 @@ const submit_rc = () => {
         status: "Unpayed"
     };
 
-    storage_rc.push(new_data);
+    const new_data_drc = {
+        id: Date.now(),
+        id_rc: new_data.id,
+        date: v_date,
+        desc: 'Init',
+        type: 'CREDIT',
+        amount: v_amount
+    };
 
+    if (status_edit) {
+        const index_drc = storage_drc.findIndex(item => item.id_rc === id_tr);
+        const status = storage_rc[index_tr].status;
+
+        new_data.id = id_tr;
+        new_data.status = status;
+
+        new_data_drc.id = storage_drc[index_drc].id;
+        new_data_drc.id_rc = id_tr;
+
+        storage_drc[index_drc] = new_data_drc;
+        storage_rc[index_tr] = new_data;
+
+        input_h2_rc.textContent = 'Add Receivables';
+
+        input_typeRc.style.display = 'block';
+        input_amountRc.style.display = 'block';
+        input_interestRc.style.display = 'block';
+        input_termRc.style.display = 'block';
+
+        input_typeRcL.style.display = 'block';
+        input_amountRcL.style.display = 'block';
+        input_interestRcL.style.display = 'block';
+        input_termRcL.style.display = 'block';
+
+        status_edit = false;
+        id_tr = null;
+        index_tr = null;
+    } else {
+        storage_rc.push(new_data);
+        storage_drc.push(new_data_drc);
+    }
+
+    save_drc(storage_drc);
     save_rc(storage_rc);
 };
 
@@ -59,16 +110,46 @@ const action_button = () => {
         switch (true) {
             case e.target.classList.contains('deleteRc'):
                 const storage_rc = JSON.parse(localStorage.getItem(SK_RC)) || [];
+                const storage_drc = JSON.parse(localStorage.getItem(SK_DRC)) || [];
                 const del_storageRc = storage_rc.filter(item => item.id !== Number(e.target.dataset.id));
+                const del_storagedRc = storage_drc.filter(item => item.id_rc !== Number(e.target.dataset.id));
 
+                save_drc(del_storagedRc);
                 save_rc(del_storageRc);
                 renderTable();
                 break;
             case e.target.classList.contains('editRc'):
+                const id_edit = Number(e.target.dataset.id);
+                const storage_rcE = JSON.parse(localStorage.getItem(SK_RC)) || [];
+                const storage_drcE = JSON.parse(localStorage.getItem(SK_DRC)) || [];
+                const data_edit = storage_rcE.find(item => item.id === id_edit);
+                const data_editDrc = storage_drcE.find(item => item.id_rc === id_edit);
 
+                input_dateRc.value = data_editDrc.date;
+                input_descRc.value = data_edit.desc;
+                input_typeRc.value = data_edit.type;
+                input_amountRc.value = data_edit.amount;
+                input_interestRc.value = data_edit.interest;
+                input_termRc.value = data_edit.term;
+
+                input_typeRc.style.display = 'none';
+                input_amountRc.style.display = 'none';
+                input_interestRc.style.display = 'none';
+                input_termRc.style.display = 'none';
+
+                input_typeRcL.style.display = 'none';
+                input_amountRcL.style.display = 'none';
+                input_interestRcL.style.display = 'none';
+                input_termRcL.style.display = 'none';
+
+                input_h2_rc.textContent = 'Edit';
+                index_tr = storage_rcE.findIndex(item => item.id === id_edit);
+                id_tr = id_edit;
+                status_edit = true;
                 break;
             case e.target.classList.contains('detailRc'):
-
+                const id_detail = e.target.dataset.id;
+                window.location.href = `detail.html?id=${id_detail}`;
                 break;
         }
     })
@@ -92,8 +173,8 @@ const renderTable = () => {
             createTd(item.desc),
             createTd(item.type),
             createTd(formatRupiah(item.amount)),
-            createTd(item.interest),
-            createTd(item.term),
+            createTd(item.interest + '%'),
+            createTd(item.term + ' Month'),
             createTd(item.status)
         )
 
@@ -108,14 +189,18 @@ const renderTable = () => {
     })
 }
 
-form_rc.addEventListener('submit', (e) => {
-    e.preventDefault();
+if (form_rc) {
+    form_rc.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    submit_rc();
-    renderTable();
-    action_button();
-    form_rc.reset();
-});
+        submit_rc();
+        renderTable();
+        action_button();
+        form_rc.reset();
+    });
+} else {
+    
+}
 
 renderTable();
 action_button();
